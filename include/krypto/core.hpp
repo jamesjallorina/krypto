31e3610de99be973e805794d945720e4e985b33f
jamesjallorina <jamesmarjun.jallorina@yahoo.com.ph>
2021-04-10 21:06:25 +0800

    add: core header file
    Signed-off-by: jamesjallorina <jamesmarjun.jallorina@yahoo.com.ph>

---
+++ include/krypto/core.hpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2021-present cppnetwork
+// Copyright (c) 2021-present James Marjun Jallorina
+// All Rights Reserved
+//
+// Distributed under the "MIT License". See the accompanying LICENSE.rst file.
+
+#pragma once
+
+#include <krypto/detail/scope_file_descriptor.hpp>
+#include <krypto/detail/scope_thread.hpp>
+#include <krypto/detail/tcp_client.hpp>
+#include <krypto/detail/tcp_server.hpp>
+#include <krypto/detail/ssl_server.hpp>
+#include <krypto/detail/ssl_client.hpp>
+#include <krypto/detail/basic_handle.hpp>
+#include <krypto/detail/ssl_helper.hpp>
+#include <krypto/detail/connector.hpp>
+#include <krypto/detail/acceptor.hpp>
--- include/krypto/krpyto.hpp
+++ include/krypto/krpyto.hpp
@@ -7,16 +7,7 @@
 #ifndef INCLUDE_KRYPTO_HPP_
 #define INCLUDE_KRYPTO_HPP_
 
-#include <krypto/detail/scope_file_descriptor.hpp>
-#include <krypto/detail/scope_thread.hpp>
-#include <krypto/detail/tcp_client.hpp>
-#include <krypto/detail/tcp_server.hpp>
-#include <krypto/detail/ssl_server.hpp>
-#include <krypto/detail/ssl_client.hpp>
-#include <krypto/detail/basic_handle.hpp>
-#include <krypto/detail/ssl_helper.hpp>
-#include <krypto/detail/connector.hpp>
-#include <krypto/detail/acceptor.hpp>
+#include <krypto/core.hpp>
 
 namespace krypto
 {
